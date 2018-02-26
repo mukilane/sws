0244f2f5a4410fb013f023356cd290ae49e7c8ac
mukilane <mukilane@gmail.com>
2018-02-26 20:38:20 +0530

    bt test

--- ble.py
+++ ble.py
@@ -21,10 +21,20 @@ from time import sleep
 
 from beacontools import BeaconScanner, EddystoneFilter, EddystoneTLMFrame
 
+def callback(bt_addr, rssi, packet, additional_info):
+        """Callback for when message arrives
+        
+        Arguments:
+            bt_addr {[type]} -- [description]
+            rssi {[type]} -- [description]
+            packet {[type]} -- [description]
+            additional_info {[type]} -- [description]
+        """
+        print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
 
 class BLE(object):
     
-    def __init__(self, callback):
+    def __init__(self):
         self.scanner = BeaconScanner(
             callback,
             packet_filter=EddystoneTLMFrame
@@ -36,20 +46,13 @@ class BLE(object):
             1, 1, 1, 200)
         self.service.stop_advertising()
 
-    def callback(bt_addr, rssi, packet, additional_info):
-        """Callback for when message arrives
-        
-        Arguments:
-            bt_addr {[type]} -- [description]
-            rssi {[type]} -- [description]
-            packet {[type]} -- [description]
-            additional_info {[type]} -- [description]
-        """
-        print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
-
     def scan(self):
         """Scans for beacons"""
 
         self.scanner.start()
         sleep(10)
         self.scanner.stop()
+
+if __name__ == "__main__":
+    ble = BLE()
+    ble.scan()
