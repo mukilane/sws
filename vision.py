9e678d02c39d4d3d031c0a04e3667aa528b16875
mukilane <mukilane@gmail.com>
2018-03-25 12:43:40 +0530

    vision deepai

--- vision.py
+++ vision.py
@@ -1,14 +1,38 @@
 import io
 import os
+import time
 import google.cloud.vision
+import picamera
+import requests
 
-vision_client = google.cloud.vision.ImageAnnotatorClient()
+def TakePic():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (640, 480)
+        camera.start_preview()
+        time.sleep(2)
+        camera.capture('./test.jpg')
+        camera.stop_preview()
 
-with io.open('./test.jpg', 'rb') as image_file:
-    content = image_file.read()
+#vision_client = google.cloud.vision.ImageAnnotatorClient()
 
-image = google.cloud.vision.types.Image(content=content)
-response = vision_client.label_detection(image=image)
+#with io.open('./test.jpg', 'rb') as image_file:
+    #content = image_file.read()
 
-for label in response.label_annotations:
-    print(label.description)
+#image = google.cloud.vision.types.Image(content=content)
+#response = vision_client.label_detection(image=image)
+
+#for label in response.label_annotations:
+#    print(label.description)
+
+def DeepAi():
+    r = requests.post(
+      "https://api.deepai.org/api/densecap",
+      files={
+        'image': open('./test.jpg', 'rb'),
+      },
+      headers={'api-key' : '7bb8a18d-b8ee-48d8-a576-eebd38a9da5f'}
+    )
+    print(r.json())
+
+TakePic()
+DeepAi()
